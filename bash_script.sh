e8113f54d5f4f60180d5f199a8165f227d45f5b6
brunosgatti <brunogatti@usp.br>
2023-10-07 10:27:54 -0300

    added file Q2

--- bash_script.sh
+++ bash_script.sh
@@ -2,4 +2,4 @@ pwd #Demonstrando o diretório em que estamos
 
 ls /home > lista_HOME.txt #Listando o conteúdo de /home e redirecionando a resposta para o arquivo lista_HOME.txt
 
-date
+date #Imprimindo a data completa na tela
